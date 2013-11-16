7c847dd52218d35c1ca4ed35194b8df1cfb92cc4
Wolf <lailavrazda1979@gmail.com>
2013-11-16 03:35:59 -0600

    Changed autogen.sh to not run configure after generating it. Pet peeve.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,3 @@ bs_dir="$(dirname $(readlink -f $0))"
 touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
 
-if test -z "$NOCONFIGURE" ; then
-	echo 'Configuring...'
-	"$bs_dir"/configure "$@"
-fi
